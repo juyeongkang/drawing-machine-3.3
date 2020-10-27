17827ad46e47dad6a37a88afda48278ab7266507
Juyeong <landsofg@gmail.com>
2020-10-26 22:51:45 -0700

    added

--- sketch.js
+++ sketch.js
@@ -2,46 +2,71 @@
   let backgroundColor = 200;
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(backgroundColor);
-
-
-  strokeWeight(10);
-
-}
-136, 201, 242
-
-function draw() {
-
-  if (mouseIsPressed) {
-    backgroundColor -= 3;
+  function setup() {
+    createCanvas(600, 600);
     background(backgroundColor);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    array.push([mouseX, mouseY]);
+
+    drawGrid();
 
   }
 
-}
+  function draw() {
 
-function keyTyped(){
+    if (mouseIsPressed) {
+      backgroundColor -= 3;
+      background(backgroundColor);
+      // line(mouseX, mouseY, pmouseX, pmouseY);
+      array.push([mouseX, mouseY]);
 
-  if (key === 's'){
-    // save this image
-    saveCanvas('fileName','png');
-  } else if (key === 'd'){
-    //  display image
-    background(2, 72, 115);
-
-    beginShape();
-    for(let i = 0; i < array.length; i++){
-      // line(array[i][0], array[i][1], array[i + 1][0] , array[i + 1][1]);
-      curveVertex(array[i][0], array[i][1])
     }
-    endShape();
-
 
   }
 
-  return false;
-}
+  function keyTyped() {
+
+    if (key === 's') {
+      // save this image
+      saveCanvas('fileName', 'png');
+    } else if (key === 'd') {
+      //  display image
+      background(2, 72, 115);
+
+      beginShape();
+      for (let i = 0; i < array.length; i++) {
+        // line(array[i][0], array[i][1], array[i + 1][0] , array[i + 1][1]);
+        curveVertex(array[i][0], array[i][1])
+      }
+      endShape();
+
+
+    }
+
+    return false;
+
+  }
+
+  function mousePressed() {
+    array = [];
+    backgroundColor = 255;
+  }
+
+  function drawGrid() {
+    numCells = 20;
+    fillColor = 255;
+    strokeWeight(0);
+
+
+    for (let i = 0; i <= width; i += width / numCells) {
+      for (let j = 0; j <= height; j += height / numCells) {
+        if (fillColor === 255) {
+          fillColor = 200;
+        } else {
+          fillColor = 255;
+        }
+        fill(fillColor);
+        rect(i, j, width / numCells, height / numCells);
+      }
+    }
+
+    strokeWeight(5);
+  }
